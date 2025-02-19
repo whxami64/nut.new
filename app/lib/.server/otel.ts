c52ae1f9daead6c542bedd1bb8e9d167ec3e5c67
Chris Toshok <ctoshok@replay.io>
2025-02-18 16:18:03 -0800

    Fix otel errors (#31)
    Running the prior code in wrangler (but _not_ `npm run dev`) was generating errors during otel initialization.  I was ultimately unable to figure out why, or how to fix them:
    
    ```
      (warn) Initializing OpenTelemetry
      (error) Error initializing OpenTelemetry TypeError: Class constructor OTLPExporterBase2 cannot be invoked without 'new'
    ```
    
    In the end, it was easier to just boil the ocean a little:  With claude's help, write a custom otlp exporter and use that.  Also use BasicTracerProvider instead of WebTracerProvider.
    
    This version is enough to generate spans from within local wrangler.

--- app/lib/.server/otel.ts
+++ app/lib/.server/otel.ts
@@ -1,12 +1,188 @@
-import { Resource } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { SpanStatusCode, type Attributes, context, trace } from '@opentelemetry/api';
 import type { Tracer } from '@opentelemetry/api';
+import { SpanStatusCode, type Attributes, context, trace } from '@opentelemetry/api';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import type { ExportResult } from '@opentelemetry/core';
+import { ExportResultCode } from '@opentelemetry/core';
+import type { ExportServiceError, OTLPExporterConfigBase } from '@opentelemetry/otlp-exporter-base';
+import { OTLPExporterError } from '@opentelemetry/otlp-exporter-base';
+import { createExportTraceServiceRequest } from '@opentelemetry/otlp-transformer';
+import { Resource } from '@opentelemetry/resources';
+import type { SpanExporter, ReadableSpan } from '@opentelemetry/sdk-trace-base';
+import { ConsoleSpanExporter, SimpleSpanProcessor, BasicTracerProvider } from '@opentelemetry/sdk-trace-base';
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+
 import type { AppLoadContext } from '@remix-run/cloudflare';
+
+// used to implement concurrencyLimit in the otlp exporter
+class Semaphore {
+  private _permits: number;
+  private _tasks: (() => void)[] = [];
+
+  constructor(permits: number) {
+    this._permits = permits;
+  }
+
+  async acquire(): Promise<void> {
+    if (this._permits > 0) {
+      this._permits -= 1;
+      return Promise.resolve();
+    }
+
+    return new Promise<void>((resolve) => {
+      this._tasks.push(resolve);
+    });
+  }
+
+  release(): void {
+    this._permits += 1;
+
+    const nextTask = this._tasks.shift();
+
+    if (nextTask) {
+      this._permits -= 1;
+      nextTask();
+    }
+  }
+}
+
+interface OTLPExporterConfig extends OTLPExporterConfigBase {
+  retryCount?: number;
+  retryIntervalMillis?: number;
+}
+
+const defaultOptions = {
+  url: 'https://api.honeycomb.io/v1/traces',
+  concurrencyLimit: 5,
+  timeoutMillis: 5000,
+  headers: {},
+  retryCount: 3,
+  retryIntervalMillis: 100,
+} as const;
+
+export class OTLPExporter implements SpanExporter {
+  private readonly _config: OTLPExporterConfig;
+  private _shutdownOnce: boolean;
+  private _activeExports: Promise<void>[];
+  private _semaphore: Semaphore;
+
+  constructor(config: OTLPExporterConfig) {
+    this._config = {
+      ...config,
+      headers: { ...config.headers },
+    };
+    this._shutdownOnce = false;
+    this._activeExports = [];
+    this._semaphore = new Semaphore(this._config.concurrencyLimit || defaultOptions.concurrencyLimit);
+  }
+
+  export(spans: ReadableSpan[], resultCallback: (result: ExportResult) => void): void {
+    if (this._shutdownOnce) {
+      console.warn('Exporter has been shutdown, skipping export.');
+      resultCallback({ code: ExportResultCode.FAILED });
+
+      return;
+    }
+
+    const exportPromise = this._export(spans);
+    this._activeExports.push(exportPromise);
+
+    // Clean up completed exports
+    exportPromise
+      .then(() => {
+        resultCallback({ code: ExportResultCode.SUCCESS });
+      })
+      .catch((error) => {
+        console.warn('CustomOTLPSpanExporter export failed:', error);
+        resultCallback({ code: ExportResultCode.FAILED, error });
+      })
+      .finally(() => {
+        const index = this._activeExports.indexOf(exportPromise);
+
+        if (index !== -1) {
+          this._activeExports.splice(index, 1);
+        }
+      });
+  }
+
+  private async _export(spans: ReadableSpan[]): Promise<void> {
+    if (spans.length === 0) {
+      return;
+    }
+
+    let currentRetry = 0;
+
+    // types involving config objects with optional fields are such a pain, hence the defaults here.
+    const { retryCount = defaultOptions.retryCount, retryIntervalMillis = defaultOptions.retryIntervalMillis } =
+      this._config;
+
+    while (currentRetry < retryCount!) {
+      try {
+        await this._semaphore.acquire();
+
+        try {
+          await this._sendSpans(spans);
+          return;
+        } finally {
+          this._semaphore.release();
+        }
+      } catch (error) {
+        currentRetry++;
+
+        if (currentRetry === retryCount) {
+          throw new OTLPExporterError(
+            `Failed to export spans after ${retryCount} retries.  most recent error is ${error instanceof Error ? error.toString() : error}`,
+          );
+        }
+
+        // Wait before retrying
+        await new Promise((resolve) => setTimeout(resolve, retryIntervalMillis * currentRetry));
+      }
+    }
+  }
+
+  private async _sendSpans(spans: ReadableSpan[]): Promise<void> {
+    const {url = defaultOptions.url, timeoutMillis = defaultOptions.timeoutMillis, headers = defaultOptions.headers } = this._config;
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeoutMillis);
+
+    const exportMessage = createExportTraceServiceRequest(spans, {
+      useHex: true,
+      useLongBits: false,
+    });
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers,
+        },
+        body: JSON.stringify(exportMessage),
+        signal: controller.signal,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } finally {
+      clearTimeout(timeoutId);
+    }
+  }
+
+  async shutdown(): Promise<void> {
+    if (this._shutdownOnce) {
+      console.warn('Exporter has already been shutdown.');
+      return;
+    }
+
+    this._shutdownOnce = true;
+    await this.forceFlush();
+  }
+
+  async forceFlush(): Promise<void> {
+    await Promise.all(this._activeExports);
+  }
+}
 
 export function createTracer(context: AppLoadContext) {
   const honeycombApiKey = (context.cloudflare.env as any).HONEYCOMB_API_KEY;
@@ -17,10 +193,10 @@ export function createTracer(context: AppLoadContext) {
     return undefined;
   }
 
-  console.warn('Initializing OpenTelemetry');
+  console.info('Initializing OpenTelemetry');
 
   try {
-    const exporter = new OTLPTraceExporter({
+    const exporter = new OTLPExporter({
       url: 'https://api.honeycomb.io/v1/traces',
       headers: {
         'x-honeycomb-team': honeycombApiKey,
@@ -33,7 +209,7 @@ export function createTracer(context: AppLoadContext) {
       [ATTR_SERVICE_VERSION]: `${__APP_VERSION}; ${__COMMIT_HASH}`,
     });
 
-    const provider = new WebTracerProvider({
+    const provider = new BasicTracerProvider({
       resource,
       spanProcessors: [new SimpleSpanProcessor(exporter), new SimpleSpanProcessor(new ConsoleSpanExporter())],
     });
@@ -63,6 +239,7 @@ export function ensureTracer() {
   if (!tracer) {
     tracer = trace.getTracerProvider().getTracer('nut-server');
   }
+
   return tracer;
 }
 
