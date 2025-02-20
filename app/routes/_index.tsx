38d389a42ee4046b143e543cbf8064ca278fc75b
Brian Hackett <bhackett1024@gmail.com>
2025-02-20 11:27:22 -0800

    Wrap chat component in <Suspense>

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import { json, type MetaFunction } from '@remix-run/cloudflare';
+import { Suspense } from 'react';
 import { ClientOnly } from 'remix-utils/client-only';
 import { BaseChat } from '~/components/chat/BaseChat';
 import { Chat } from '~/components/chat/Chat.client';
@@ -11,12 +12,16 @@ export const meta: MetaFunction = () => {
 
 export const loader = () => json({});
 
+const Nothing = () => null;
+
 export default function Index() {
   return (
     <div className="flex flex-col h-full w-full bg-bolt-elements-background-depth-1">
       <BackgroundRays />
       <Header />
-      <ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly>
+      <Suspense fallback={<Nothing />}>
+        <ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly>
+      </Suspense>
     </div>
   );
 }
